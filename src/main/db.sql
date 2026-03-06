7be4b915369c67253b0eecea4086e5a39f2651ad
Daniel Dimitrov <danieldimitrov86@gmail.com>
2026-03-06 14:54:54 -0600

    Added DB schema and table creation SQL

---
+++ src/main/db.sql
@@ -0,0 +1,24 @@
+CREATE TABLE sources (
+    id int IDENTITY(1,1) AUTO_INCREMENT PRIMARY KEY,
+    filename varchar(255) NOT NULL,
+    imported_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE words (
+    id int IDENTITY(1,1) AUTO_INCREMENT PRIMARY KEY,
+    word varchar(255) NOT NULL,
+    general_count int NOT NULL,
+    start_count int NOT NULL,
+    end_count int NOT NULL,
+    source_id int NOT NULL,
+    FOREIGN KEY (source_id) REFERENCES sources(id)
+);
+
+CREATE TABLE bigrams (
+    first_word_id int NOT NULL,
+    second_word_id int NOT NULL,
+    count int NOT NULL,
+    PRIMARY KEY (first_word_id, second_word_id),
+    FOREIGN KEY (first_word_id) REFERENCES words(id),
+    FOREIGN KEY (second_word_id) REFERENCES words(id)
+);
