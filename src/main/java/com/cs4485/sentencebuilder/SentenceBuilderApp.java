45064b11d35a33eeb126256fb0608985d7d636b6
Connor Harris <CWHarris0812@gmail.com>
2026-04-02 20:38:35 -0500

    Created the initial UI

--- src/main/java/com/cs4485/sentencebuilder/SentenceBuilderApp.java
+++ src/main/java/com/cs4485/sentencebuilder/SentenceBuilderApp.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class SentenceBuilderApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(SentenceBuilderApp.class.getResource("main-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 900, 650);
+        stage.setTitle("Sentence Builder");
         stage.setScene(scene);
         stage.show();
     }
