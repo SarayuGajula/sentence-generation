c131034240b671e4fac84798968446951f25eb98
Joe Su <joesu2005@gmail.com>
2026-04-10 15:19:46 -0500

    Added selection based on probability

--- src/main/java/com/cs4485/sentencebuilder/Generator.java
+++ src/main/java/com/cs4485/sentencebuilder/Generator.java
@@ -7,9 +7,12 @@ package com.cs4485.sentencebuilder;
  *      Get top 5 words, weight them with probabilities and randomly pick one
  * @author Joe Su
  * 04/02/2026 - Initial creation, needs update for capitalization and probabilities - Joe
+ * 04/10/2026 - Added selection based on probability - Joe
  */
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import com.cs4485.sentencebuilder.model.entity.Bigram;
 import com.cs4485.sentencebuilder.model.dao.BigramDAO;
@@ -29,6 +32,7 @@ public class Generator {
             Bigram commonBigram = bigramDAO.getTopKMostCommonBigramsStartingWithWord(1, word).get(0);
             word = commonBigram.getSecondWord();
             count++;
+
         }
         sb.append(word);
 
@@ -39,11 +43,37 @@ public class Generator {
         StringBuilder sb = new StringBuilder(first);
         String word = first.toLowerCase().replaceAll(punctRegex + "$", ""); // word to display next
         int count = 0; // for debugging
+        Random r = new Random();
+
         while(!word.matches(punctRegex) && count < 50){
             if(!word.equals(first)){ sb.append(" ").append(word); }
 
             List<Bigram> commonBigram = bigramDAO.getTopKMostCommonBigramsStartingWithWord(5, word);
-            // probability stuff, update word
+            int index = -1;
+            List<Integer> countList = new ArrayList<>();
+            int totalCount = 0;
+            for(Bigram bigram : commonBigram){
+                countList.add(bigram.getCount());
+                totalCount += bigram.getCount();
+            }
+
+            int prob = r.nextInt(totalCount);
+            int thresh = 0;
+
+            for(int i = 0; i < countList.size(); i++){
+                int p = countList.get(i);
+                thresh+=p;
+                if(prob < thresh){
+                    index = i;
+                    break;
+                }
+            }
+
+            if(thresh >= totalCount || index == -1){
+                throw new Error("Probability selection failed!");
+            }
+
+            word = commonBigram.get(index).getSecondWord();
             count++;
         }
         sb.append(word);
