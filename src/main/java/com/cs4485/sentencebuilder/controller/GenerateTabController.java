5785df51bb876a136b777b557106ec593cb878ca
Joe Su <joesu2005@gmail.com>
2026-04-17 10:47:28 -0500

    Added Generation algorithms to respective tab

--- src/main/java/com/cs4485/sentencebuilder/controller/GenerateTabController.java
+++ src/main/java/com/cs4485/sentencebuilder/controller/GenerateTabController.java
@@ -5,17 +5,23 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
+import com.cs4485.sentencebuilder.Generator;
+
 /**
  * Controller for the generate tab
  * @author Connor Harris
  * @author Daniel Dimitrov
+ * @author Joe Su
  * 4/2/2026 - Dummy version implemented that doesn't actually do anything yet - Connor Harris
  * 4/10/2026 - Extracted all fields and functions for generate tab from Connor's MainController - Daniel Dimitrov
+ * 4/17/2026 - Added sentence generation logic - Joe Su
  */
 public class GenerateTabController {
     @FXML private TextField startWordField;
     @FXML private ComboBox<String> algorithmCombo;
     @FXML private TextArea generatedOutput;
+
+    private Generator generator;
 
     @FXML
     protected void onGenerate() {
@@ -25,13 +31,26 @@ public class GenerateTabController {
             return;
         }
         // TODO: call sentence generation logic here
-        generatedOutput.setText("Generation not yet implemented.");
+        String selected = algorithmCombo.getSelectionModel().getSelectedItem();
+        String sentence;
+        if("Most Frequent".equals(selected)) {
+            sentence = generator.commonGenerator(startWord);
+        }
+        else if("Random Weighted".equals(selected)){
+            sentence = generator.topFiveWordsGenerator(startWord);
+        }
+        else{
+            sentence = "Please pick a selection model.";
+        }
+        generatedOutput.setText(sentence);
     }
 
     @FXML
     public void initialize() {
         algorithmCombo.getItems().addAll("Most Frequent", "Random Weighted");
         algorithmCombo.setValue("Most Frequent");
+
+        generator = new Generator();
 
 //        wordSortCombo.getItems().addAll(
 //                "Alphabetical",
